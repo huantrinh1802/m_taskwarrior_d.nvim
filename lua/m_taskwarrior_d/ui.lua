2802197f29f9bac0070b1e0c02da9ca267d79334
Ben Trinh <6076614+huantrinh1802@users.noreply.github.com>
2024-03-24 21:01:02 +1100

    [modify] Extract popup logic into a separate ui module - Extracted logic for popup window creation and triggering into a new ui module - Replaced the previous implementation in view_task() with a call to the new  function trigger_hover() in the ui module - Ensured required parameters are passed in for the new function call and ensured  existing logic is not disturbed

---
+++ lua/m_taskwarrior_d/ui.lua
@@ -0,0 +1,35 @@
+local M = {}
+
+function M.trigger_hover(contents)
+  local Popup = require("nui.popup")
+  local event = require("nui.utils.autocmd").event
+  local autocmd = require("nui.utils.autocmd")
+  local max_width = 0
+  for _, content in ipairs(contents) do
+    max_width = math.max(max_width, #content)
+  end
+  local popup = Popup({
+    enter = false,
+    focusable = true,
+    border = {
+      style = "rounded",
+    },
+    relative = "cursor",
+    position = 0,
+    size = {
+      width = max_width + 1,
+      height = #contents,
+    },
+  })
+
+  -- mount/open the component
+  local bufnr = vim.api.nvim_get_current_buf()
+  autocmd.buf.define(bufnr, { event.CursorMoved, event.BufEnter }, function()
+    popup:unmount()
+  end, { once = true })
+  popup:mount()
+
+  vim.api.nvim_buf_set_lines(popup.bufnr, 0, 1, false, contents)
+end
+
+return M
