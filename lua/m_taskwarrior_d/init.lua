9f75755486ffaebd77ce1fc6abee340e4147bf40
Ben Trinh <6076614+huantrinh1802@users.noreply.github.com>
2024-03-24 22:07:39 +1100

    [dev] Add TWRun function and minor adjustments
    - Added TWRun function in m_taskwarrior_d/init.lua
    - Moved require("nui.input") line in run_with_current function
    - Provided two ways to use TWRun command and detailed usage explained in README
    .md

--- lua/m_taskwarrior_d/init.lua
+++ lua/m_taskwarrior_d/init.lua
@@ -117,10 +117,9 @@ function M.view_task()
 end
 
 function M.run_with_current()
-  local Input = require("nui.input")
-  -- local event = require("nui.utils.autocmd").event
   local current_line, line_number = M.utils.get_line()
   local _, uuid = M.utils.extract_uuid(current_line)
+  local Input = require("nui.input")
   local input = Input({
     relative = "cursor",
     position = 0,
@@ -147,6 +146,60 @@ function M.run_with_current()
   })
   input:mount()
 end
+
+local function split_by_newline(input)
+  local lines = {}
+  for line in input:gmatch("[^\n]+") do
+    table.insert(lines, line)
+  end
+  table.insert(lines, #lines, string.rep("-", #lines[1]))
+  return lines
+end
+
+function M.run_task(args)
+  local command = "task " .. table.concat(args, " ")
+  if #args == 0 then
+    local Input = require("nui.input")
+    local input = Input({
+      relative = "cursor",
+      position = 0,
+      size = {
+        width = 100,
+      },
+      border = {
+        style = "single",
+        text = {
+          top = "Run task",
+          top_align = "center",
+        },
+      },
+      win_options = {
+        winhighlight = "Normal:Normal,FloatBorder:Normal",
+      },
+    }, {
+      on_submit = function(value)
+        M.run_task({ value })
+      end,
+    })
+    input:mount()
+  else
+    local _, result = M.task.execute_taskwarrior_command(command, true)
+    if #result == 0 then
+      print("No task found")
+      return
+    end
+    local task_commands_not_to_display = { "add", "mod", "del", "purge" }
+    for _, keyword in ipairs(task_commands_not_to_display) do
+      if string.find(command, keyword) then
+        print(result)
+        return
+      end
+    end
+    result = split_by_newline(result)
+    M.ui.trigger_hover(result)
+  end
+end
+
 local function process_opts(opts)
   if opts ~= nil then
     for k, v in pairs(opts) do
@@ -219,6 +272,9 @@ function M.setup(opts)
   vim.api.nvim_create_user_command("TWRunWithCurrent", function()
     M.run_with_current()
   end, {})
+  vim.api.nvim_create_user_command("TWRun", function(args)
+    M.run_task(args.fargs)
+  end, { nargs = "*" })
 end
 
 return M
